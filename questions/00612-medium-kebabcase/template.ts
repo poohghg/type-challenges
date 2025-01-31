8dd5fbd02a6b6a25718838932a71465b5799d652
khg <poohghg@naver.com>
2025-02-01 06:01:06 +0900

    feat: 문제 풀이

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -4,3 +4,16 @@ type KebabCase<S extends string> =
       ? `${Uncapitalize<Start>}${KebabCase<Rest>}`
       : `${Uncapitalize<Start>}-${KebabCase<Rest>}`
     : S
+
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+]
