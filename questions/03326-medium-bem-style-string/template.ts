84c096df68d6d4c1caadc030243a53a65dc055e8
khg <poohghg@naver.com>
2025-02-11 17:06:00 +0900

    feat: 문제 풀이

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,6 +1,6 @@
 type IsEmptyArray<T extends string[]> = T['length'] extends 0 ? true : false
 
-type UnionArray<T extends string[], Prefix extends string> = IsEmptyArray<T> extends true ? '' : `${Prefix}${T[number]}`
+type UnionString<T extends string[], Prefix extends string> = IsEmptyArray<T> extends true ? '' : `${Prefix}${T[number]}`
 
 type BEM<B extends string, E extends string[], M extends string[]> =
-  `${B}${UnionArray<E, '__'>}${UnionArray<M, '--'>}`
+  `${B}${UnionString<E, '__'>}${UnionString<M, '--'>}`
