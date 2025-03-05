48f613d9dd71a5e5d8f1942db3c582075e06b671
khg <poohghg@naver.com>
2025-03-05 16:43:53 +0900

    feat: 문제 풀이

--- questions/29785-medium-deep-omit/template.ts
+++ questions/29785-medium-deep-omit/template.ts
@@ -1,1 +1,5 @@
-type DeepOmit = any
+type DeepOmit<T extends Record<string, any>, U extends string> = U extends `${infer Head}.${infer Rest}`
+  ? {
+      [K in keyof T]: K extends Head ? DeepOmit<T[K], Rest> : T[K]
+    }
+  : Omit<T, U>
--- questions/30301-medium-isodd/template.ts
+++ questions/30301-medium-isodd/template.ts
@@ -1,1 +1,26 @@
-type IsOdd<T extends number> = any
+type IsOddOImpl<T extends number[]> =
+  T['length'] extends 0 | 1 | 2
+    ? T['length'] extends 1
+      ? true
+      : false
+    : T extends [number, ...infer Middle extends number[], number]
+      ? IsOddOImpl<Middle>
+      : never
+
+type IsOdd<T extends number, Last = LastString<T>, Count extends number[] = []> =
+  `${Count['length']}` extends Last
+    ? IsOddOImpl<Count>
+    : IsOdd<T, Last, [...Count, 0]>
+
+type LastString<T extends string | number> =
+  `${T}` extends `${infer Head}${infer Rest}`
+    ? Rest extends ''
+      ? Head
+      : LastString<Rest>
+    : T
+
+{
+  let a: IsOdd<4>
+  let b: IsOdd<2.3>
+  let c: LastString<2.3>
+}
