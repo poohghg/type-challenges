281018d5d7454162d239ccd2e2c0b35d4308e257
khg <poohghg@naver.com>
2025-10-20 16:45:49 +0900

    feat: 문제 풀이

--- questions/02946-medium-objectentries/test-cases.ts
+++ questions/02946-medium-objectentries/test-cases.ts
@@ -15,5 +15,3 @@ type cases = [
   Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>,
   Expect<Equal<ObjectEntries<{ key: string | undefined }>, ['key', string | undefined]>>,
 ]
-
-let a: ObjectEntries<Partial<Model>>
