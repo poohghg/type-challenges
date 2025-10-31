caa004f78d180d28d45b458240a9e39d25200a45
khg <poohghg@naver.com>
2025-10-31 16:33:38 +0900

    feat: 문제 풀이

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -4,8 +4,8 @@ type FlattenDepth<
   U extends any[] = [],
 > = U['length'] extends S
   ? T
-  : T extends [infer F, ...infer R]
+  : T extends [infer F, ...infer Rest]
     ? F extends any[]
-      ? [...FlattenDepth<F, S, [...U, 1]>, ...FlattenDepth<R, S, U>]
-      : [F, ...FlattenDepth<R, S, U>]
+      ? [...FlattenDepth<F, S, [...U, 0]>, ...FlattenDepth<Rest, S, U>]
+      : [F, ...FlattenDepth<Rest, S, U>]
     : T
