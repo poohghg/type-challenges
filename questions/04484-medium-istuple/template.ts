ce44b1ef7abf726862539eb38525f8dc81ddc981
khg <poohghg@naver.com>
2025-11-19 18:28:41 +0900

    feat: 문제 풀이

--- questions/04484-medium-istuple/template.ts
+++ questions/04484-medium-istuple/template.ts
@@ -1,1 +1,9 @@
-type IsTuple<T> = any
+// Tuple is a kind of Array with fixed length.
+type IsTuple<T> =
+  [T] extends [never]
+    ? false
+    : T extends readonly any[]
+      ? number extends T['length']
+        ? false
+        : true
+      : false
--- questions/04499-medium-chunk/template.ts
+++ questions/04499-medium-chunk/template.ts
@@ -1,1 +1,6 @@
-type Chunk = any
+type Chunk<T extends any[], N extends number, R extends any[] = []> =
+  R['length'] extends N
+    ? [R, ...Chunk<T, N>]
+    : T extends [infer S, ...infer Rest]
+      ? Chunk<Rest, N, [...R, S]>
+      : R extends [] ? [] : [R]
